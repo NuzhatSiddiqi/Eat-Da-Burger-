da0b9de8e97dc5fff0528f364479f50bb0ff97dc
Nuzhat Siddiqi <46729206+NuzhatSiddiqi@users.noreply.github.com>
2019-08-21 15:46:30 -0400

    add test.html file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
+// Connect Node to MySQL.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
@@ -15,4 +20,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export the connection.
 module.exports = connection;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,87 +1,61 @@
+// Dependencies
 var express = require("express");
+// Import the model to use its db functions for burger.js
+var burger = require("../models/burger.js");
 
+// Create the router for the app, and export the router at the end of your file.
 var router = express.Router();
-// grabbing our models
-var db = require("../models");
-
-// get route -> index
+// Create routes and set up logic where required.
 router.get("/", function(req, res) {
-  // send us to the next get function instead.
-  res.redirect("/burgers");
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
-
-// get route, edited to match sequelize
-router.get("/burgers", function(req, res) {
-  // replace old function with sequelize function
-  db.Burger.findAll({
-    include: [db.Customer],
-    // Here we specify we want to return our burgers in ordered by ascending burger_name
-    order: [["burger_name", "ASC"]]
-  })
-    // use promise method to pass the burgers...
-    .then(function(dbBurger) {
-      // into the main index, updating the page
-      var hbsObject = {
-        burger: dbBurger
-      };
-      return res.render("index", hbsObject);
-    });
+// Add new burger to the db.
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devoured],
+    function(result) {
+      // Send back the ID of the new burger
+      res.json({ id: result.insertId });
+    }
+  );
 });
+// Set burger devoured status to true.
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-// post route to create burgers
-router.post("/burgers/create", function(req, res) {
-  // edited burger create to add in a burger_name
-  db.Burger.create({
-    burger_name: req.body.burger_name
-  })
-    // pass the result of our call
-    .then(function(dbBurger) {
-      // log the result to our terminal/bash window
-      console.log(dbBurger);
-      // redirect
-      res.redirect("/");
-    });
+  console.log("condition", condition);
+
+  burger.updateOne({ devoured: req.body.devoured }, condition, function(
+    result
+  ) {
+    if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404.
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
+// Delete burger from db.
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
 
-// put route to devour a burger
-router.put("/burgers/update", function(req, res) {
-  // If we are given a customer, create the customer and give them this devoured burger
-  if (req.body.customer) {
-    db.Customer.create({
-      customer: req.body.customer,
-      BurgerId: req.body.burger_id
-    })
-      .then(function(dbCustomer) {
-        return db.Burger.update(
-          {
-            devoured: true
-          },
-          {
-            where: {
-              id: req.body.burger_id
-            }
-          }
-        );
-      })
-      .then(function(dbBurger) {
-        res.json("/");
-      });
-  }
-  // If we aren't given a customer, just update the burger to be devoured
-  else {
-    db.Burger.update(
-      {
-        devoured: true
-      },
-      {
-        where: {
-          id: req.body.burger_id
-        }
-      }
-    ).then(function(dbBurger) {
-      res.json("/");
-    });
-  }
+  burger.deleteOne(condition, function(result) {
+    if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404.
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
 
 module.exports = router;
